89b807729c6c08eb3912cb3935b40c7e33f5ae56
Victor Amaral <canalv1kill@gmail.com>
2023-10-18 14:53:39 -0300

    Finalizei a Função Menu
    Adicionei uma Variável "Permissão" Para quando o usuário sair do menu e queira manter a lista do menu aberta, a função de ocultar o menu não seja ativada

--- static/js/script.js
+++ static/js/script.js
@@ -1,19 +1,36 @@
 
+var Permissão = 'Sim'
+
 function AtivarMenu() {
-    var menu = document.getElementById('menu');
-    menu.style.display = 'flex';
-    setTimeout(function() {
-        menu.style.left = '150px';
-    }, 0);
+    if (Permissão == 'Sim') {
+        var menu = document.getElementById('menu');
+        menu.style.display = 'flex';
+        setTimeout(function() {
+            menu.style.left = '150px';
+        }, 50);
+    }
 }
 
 function DesativarMenu() {
-    var menu = document.getElementById('menu');
-    menu.style.left = '0px';
-    setTimeout(function() {
-        menu.style.display = 'none';
-    }, 300);
+    if (Permissão == 'Sim') {
+        var menu = document.getElementById('menu');
+        menu.style.left = '-10px';
+        if (Permissão == 'Não')
+            menu.style.display = 'flex';
+    }
+}
 
+function ManterMenu () {
+    Permissão = 'Não'
+    var menu = document.getElementById('menu'); 
+    menu.style.left = '150px';
+    
+}
+
+function SairMenu() {
+    var menu = document.getElementById('menu'); 
+    menu.style.left = '-10px';
+    Permissão = 'Sim'
 }
 
 function MudarTema() {
@@ -26,7 +43,6 @@ function MudarTema() {
     }
 
     else {
-
         if (moded.innerText == 'dark_mode') {
             body.classList.toggle('darkmode')
             moded.innerText = 'light_mode'
